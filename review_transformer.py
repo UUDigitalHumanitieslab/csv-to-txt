e1c8ffe1491c0410d3f6c077bb53aa1aee7dcad0
Alex Hebing <a.h.j.hebing@uu.nl>
2019-12-06 14:46:05 +0100

    Close #1. agg_col can be index or string.

--- review_transformer.py
+++ review_transformer.py
@@ -13,15 +13,6 @@ def source(path):
             "Source '{}' is not a directory nor a file".format(path))
     return path
 
-def integer(value):
-    try: 
-        result = int(value)
-    except ValueError:
-        raise argparse.ArgumentTypeError("Value '{}' is not an integer".format(value))
-    
-    if not result > 0: raise argparse.ArgumentTypeError("Value '{}' should be 0 or higher".format(value))
-    return result    
-
 
 def parseArguments(sysArgs):
     parser = argparse.ArgumentParser(
@@ -38,8 +29,9 @@ def parseArguments(sysArgs):
 
     parser.add_argument(
         '--agg_col', '-ac',
-        dest='agg_col', type=integer,
-        help="The column to aggregate on. Rows will be divided into files named after unique values in this column"
+        dest='agg_col',
+        help='''Optional. The column to aggregate on. Can be either the name of the column as it appears in the top row, or a zero-based index. 
+            Rows will be divided into files named after unique values in this column'''
     )
 
     parsedArgs = parser.parse_args()
@@ -51,31 +43,52 @@ def main(sysArgs):
     args = parseArguments(sysArgs)
 
     if os.path.isfile(args.source):
-        process_source(args.source, args.target, int(args.agg_col))
+        process_source(args.source, args.target, args.agg_col)
     else:
         for file in os.listdir(args.source):
-            process_source(os.path.join(args.source, file), args.target, int(args.agg_col))
+            process_source(os.path.join(args.source, file),
+                           args.target, args.agg_col)
 
 
 def process_source(filepath, target_folder, agg_col):
     file_prefix = get_filename_prefix(filepath)
     target_folder = os.path.join(target_folder, file_prefix)
-    if agg_col: agg_files = []
-    
+    if agg_col:
+        agg_files = []
+
     with open(filepath) as csv_file:
         # reader = csv.DictReader(csv_file)
         csv_reader = csv.reader(csv_file, delimiter=',')
-        # Read first line and count columns
-        no_of_columns = len(next(csv_reader))
 
+        # Process first line
+        no_of_columns, agg_col = process_header(next(csv_reader), agg_col)
         if agg_col and agg_col > no_of_columns:
-            raise argparse.ArgumentTypeError("agg_col '{0}' does not exist in '{1}'".format(agg_col, filepath))
+            raise argparse.ArgumentTypeError("agg_col at index {0}' does not exist in '{1}'".format(agg_col, filepath))
 
+        # process other rows
         for row in csv_reader:
             if not agg_col:
                 process_row(row, no_of_columns, file_prefix, target_folder)
             else:
                 process_row_aggregate(row, no_of_columns, target_folder, agg_col, agg_files)
+
+
+def process_header(header, agg_col):
+    no_of_columns = len(header)
+
+    if agg_col:
+        # test if agg_col is integer
+        try:
+            agg_col = int(agg_col)
+            if not agg_col > 0:
+                raise argparse.ArgumentTypeError("agg_col '{0}' should be 0 or higher".format(agg_col))
+        except ValueError:
+            try:
+                agg_col = header.index(agg_col)
+            except ValueError:
+                raise argparse.ArgumentTypeError("agg_col '{0}' is not in the first row".format(agg_col))
+
+    return no_of_columns, agg_col
 
 
 def get_filename_prefix(source_file):
@@ -96,7 +109,7 @@ def process_row(row, no_of_columns, filename_prefix, target_folder):
         else:
             text = row[i]
 
-    filename = '{0}_{1}.txt'.format(filename_prefix, '_'.join(filename_fields))    
+    filename = '{0}_{1}.txt'.format(filename_prefix, '_'.join(filename_fields))
     export(target_folder, filename, text, 'w')
 
     filename_fields = []
@@ -112,12 +125,12 @@ def process_row_aggregate(row, no_of_columns, target_folder, agg_col, agg_files)
         agg_files.append(agg_col_value)
         export(target_folder, filename, text, 'w')
     else:
-        export(target_folder, filename, text, 'a')    
+        export(target_folder, filename, text, 'a')
 
 
 def get_filename(filename_prefix, filename_fields, agg_col, agg_files):
     if not agg_col:
-        return 
+        return
 
 
 def export(target_folder, filename, text, file_mode):
@@ -130,6 +143,7 @@ def export(target_folder, filename, text, file_mode):
         exportfile.write(text)
 
     os.chdir(orig_dir)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
